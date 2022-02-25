00a971aa6586f5a14c47de26715375cd5c0e2725
MBakr <moh.bakr27@gmail.com>
2022-02-25 22:15:03 +0200

    creating database and models

---
+++ backend/schemas.py
@@ -0,0 +1,40 @@
+import datetime as _dt
+import pydantic as _pydantic
+
+
+class _UserBase(_pydantic.BaseModel):
+    email: str
+
+
+class UserCreate(_UserBase):
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
+
+
+class User(_UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class _PokemonBase(_pydantic.BaseModel):
+    fav_pokemon: str
+    pit_name: str
+    note: str
+
+
+class PokemonCreate(_PokemonBase):
+    pass
+
+
+class Pokemon(_PokemonBase):
+    id: int
+    owner_id: int
+    date_created: _dt.datetime
+    date_last_updated: _dt.datetime
+
+    class Config:
+        orm_mode = True
